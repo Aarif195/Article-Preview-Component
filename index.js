1048e1e5d29e1726fa92da43e48ce9a16ecd065e
Aarif <adebayoabdulazeez195@gmail.com>
2024-10-21 04:03:36 -0700

    Correction of errors in codes

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// Get the button that shows the modal
+// When the user click on the share button, show the modal 
+
+// variables
+
+// let openBtn = document.querySelector('.openBtn')
+// console.log(openBtn);
+
+// let icon = document.querySelector('.icons');
+
+
+// Eventlistener
+
+// openBtn.addEventListener('click', function () {
+
+//   icon.style.display = 'block'
+// });
+
